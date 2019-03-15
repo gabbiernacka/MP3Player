8163eb0d409283ea4e38938dbcdc7a274c04668d
Gabriela <48495282+gabbiernacka@users.noreply.github.com>
2019-03-15 17:50:05 +0100

    Add files via upload

---
+++ src/module-info.java
@@ -0,0 +1,10 @@
+module MP3Player {
+    requires javafx.fxml;
+    requires javafx.controls;
+    requires javafx.media;
+    requires java.sql;
+    requires jl;
+
+    opens sample;
+    //opens sample.model;
+}
